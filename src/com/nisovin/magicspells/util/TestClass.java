876ef06a7c8500741e8c8a00fdf2e9c5f42bb3a9
Tiketal <a.uno.d.uno@gmail.com>
2017-06-08 14:54:27 -0400

    Removed Test class

--- src/com/nisovin/magicspells/util/TestClass.java
+++
@@ -1,9 +0,0 @@
-package com.nisovin.magicspells.util;
-
-public class TestClass {
-
-	public static void main(String[] args) {
-		ReflectionPlayerInfoData ref = new ReflectionPlayerInfoData(null, null, 0, null, null);
-	}
-
-}
