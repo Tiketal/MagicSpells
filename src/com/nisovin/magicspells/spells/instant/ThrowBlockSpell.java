c216db8a7faf8bbd01e71a91427439483123d5b8
Tiketal <wiiwatcher702@gmail.com>
2019-07-30 13:32:27 -0400

    Removed delay

--- src/com/nisovin/magicspells/spells/instant/ThrowBlockSpell.java
+++ src/com/nisovin/magicspells/spells/instant/ThrowBlockSpell.java
@@ -209,7 +209,7 @@ public class ThrowBlockSpell extends InstantSpell implements TargetedLocationSpe
 		public ThrowBlockMonitor(FallingBlock block, FallingBlockInfo info) {
 			this.block = block;
 			this.info = info;
-			this.task = MagicSpells.scheduleRepeatingTask(this, 5, 1);
+			this.task = MagicSpells.scheduleRepeatingTask(this, 0, 1);
 			this.prevVelocity = block.getVelocity();
 		}
 		
